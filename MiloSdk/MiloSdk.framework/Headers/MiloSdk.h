d0745aa88c5e2c8a71a489b9d9b0f0771ee6a09c
Jaleel Nazir <jaleel@glynk.com>
2019-04-26 10:41:18 +0530

    1. Added Framework

---
+++ MiloSdk/MiloSdk.framework/Headers/MiloSdk.h
@@ -0,0 +1,25 @@
+//
+//  MiloSdk.h
+//  MiloSdk
+//
+//  Created by Jaleel on 4/5/19.
+//  Copyright © 2019 Glynk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TOCropViewController.h"
+#import "TOCropView.h"
+#import "TOCropToolbar.h"
+#import "TOCropOverlayView.h"
+#import "TOActivityCroppedImageProvider.h"
+#import "TOCroppedImageAttributes.h"
+#import "TOCropScrollView.h"
+#import "TOCropViewControllerTransitioning.h"
+#import "UIImage+CropRotate.h"
+
+//! Project version number for MiloSdk.
+FOUNDATION_EXPORT double MiloSdkVersionNumber;
+
+//! Project version string for MiloSdk.
+FOUNDATION_EXPORT const unsigned char MiloSdkVersionString[];
---
+++ MiloSdk/MiloSdk.h
@@ -0,0 +1,15 @@
+//
+//  MiloSdk.h
+//  MiloSdk
+//
+//  Created by Jaleel on 4/5/19.
+//  Copyright © 2019 Glynk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MiloSdk.
+FOUNDATION_EXPORT double MiloSdkVerNum;
+
+//! Project version string for MiloSdk.
+FOUNDATION_EXPORT const unsigned char MiloSdkVerStr[];
