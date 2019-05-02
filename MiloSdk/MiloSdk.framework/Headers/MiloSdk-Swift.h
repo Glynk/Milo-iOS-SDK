89d4d8e2579da422b8bddca66fa2c185ea8052ac
Glynk <tech-core@glynk.com>
2019-05-02 15:43:11 +0530

    1. Updated Framework

--- MiloSdk/MiloSdk.framework/Headers/MiloSdk-Swift.h
+++ MiloSdk/MiloSdk.framework/Headers/MiloSdk-Swift.h
@@ -308,6 +308,14 @@ SWIFT_CLASS("_TtC7MiloSdk23GlynkRootViewController")
 
 
 
+SWIFT_CLASS("_TtC7MiloSdk10MiloButton")
+@interface MiloButton : UIButton
+- (nonnull instancetype)initWithFrame:(CGRect)frame OBJC_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
+- (void)awakeFromNib;
+@end
+
+
 
 
 
